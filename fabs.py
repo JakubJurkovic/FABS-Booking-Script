c7a81581fbb4baa514c0d4d17ad30ecf9974355b
Jakub Jurkovic <jj3003@nyu.edu>
2021-10-01 23:18:57 +0400

    Women Gym Added. Script will reattempt booking after failure

--- fabs.py
+++ fabs.py
@@ -6,7 +6,7 @@ import time, pause, datetime
 
 ####################################################
 #               FABS BOOKING BOT                   #
-#                    v0.5                          #
+#                    v0.6                          #
 # Requirements:                                    #
 # MsEdge driver & Selenium 3.14                    #
 # "local account" on FABS                          #
@@ -24,50 +24,54 @@ driver = Edge(executable_path=PATH, options=edge_options)
 actions = ActionChains(driver)
 driver.get("https://nyuad.dserec.com/online/capacity_widget")
 
+##########################################
 #              USER SETTINGS             #
 ##########################################
-username = "your username"
-password = "your password"
-facility = "1" # "gym" or "pool"
+username = "username" # Your login
+password = "password" # Your password
+facility = "facility" # "gym" or "pool" or "wogym"
 slot = 3 #slot number, indexing starts at 1
 ##########################################
 
-
-# This calculates when should the bot run
+# This calculates when should the bot perform login and booking
 if time.localtime()[3] <= 7:
 	run = time.localtime()
 elif time.localtime()[3] > 7:
 	run = time.localtime(time.time() + 24*3600)
-
 botlogin = datetime.datetime(run[0], run[1], run[2], 6, 55, 0)
 botrun = datetime.datetime(run[0], run[1], run[2], 7, 0, 1)
 
-
-# Books from Co-Ed gym
-def gym(t):
+# Navigates to desired facility
+def choosefacility(facility):
 	time.sleep(1)
-	actions.move_to_element_with_offset(driver.find_element_by_tag_name('body'), 50,600).click().perform() #clicks on the fitness center
+	if facility == "gym":
+		actions.move_to_element_with_offset(driver.find_element_by_tag_name('body'), 50,600).click().perform() # clicks on the fitness center
+	elif facility == "pool":
+		actions.move_to_element_with_offset(driver.find_element_by_tag_name('body'), 50,330).click().perform() # clicks on pool
+	elif facility == "wogym":
+		actions.move_to_element_with_offset(driver.find_element_by_tag_name('body'), 50,825).click().perform() # clicks on the WO fitness center
 	for i in range(2): # goes to the slot page of 2 days in advance
 		driver.find_element_by_xpath("/html/body/div/div/section/div/div/div/div[2]/div/div/div/div[1]/div[2]/div[3]/a[2]/i").click()
 	time.sleep(1)
-	pickslot(t) # clicks on the slot
 
-# Books for the swimming pool
-def pool(t):
-	time.sleep(1)
-	actions.move_to_element_with_offset(driver.find_element_by_tag_name('body'), 50,330).click().perform()
-	for i in range(2):
-		driver.find_element_by_xpath("/html/body/div/div/section/div/div/div/div[2]/div/div/div/div[1]/div[2]/div[3]/a[2]/i").click()
-	time.sleep(1)
-	pickslot(t)
-
-# Clicks on the slotF
+# Clicks on the slot
 def pickslot(t):
-	print("Freeze until 7AM")
-	pause.until(botrun) # Pauses until the system time is 7am and 1 second
 	driver.find_element_by_xpath("/html/body/div/div/section/div/div/div/div[2]/div/div/div/div[1]/div[2]/div[4]/div/div/div/div/div["+str(t)+"]").click() # Clicks on the slot		
 	time.sleep(1)
-	driver.find_element_by_xpath("/html/body/div/div/section/div/div/div/div[2]/div/div/div/div[2]/div/div/div/div[3]/div[2]/button[2]").click() # Confirm
+	if isitopen() == True: # Verifies if the slot is open, if not it will just close the action window
+		driver.find_element_by_xpath("/html/body/div/div/section/div/div/div/div[2]/div/div/div/div[2]/div/div/div/div[3]/div[2]/button[2]").click() # Confirm selection
+		return True # booking success
+	elif isitopen() == False:
+		driver.find_element_by_xpath("/html/body/div/div/section/div/div/div/div[2]/div/div/div/div[2]/div/div/div/div[2]/div[2]/button").click() # Close
+		return False # booking fail
+
+# Checks if the slot is open or not
+def isitopen(): 
+	txt = driver.find_element_by_xpath("/html/body/div/div/section/div/div/div/div[2]/div/div/div/div[2]/div/div/div/div[1]").text # Reads the text
+	if "will open" in txt:
+		return False
+	else:
+		return True
 
 # Logs in the user	
 def login(user,pd):
@@ -80,11 +84,18 @@ def login(user,pd):
 	driver.find_element_by_xpath("/html/body/div[2]/form/div[1]/div[2]/input").send_keys(pd) # Inserts pwd
 	driver.find_element_by_xpath("/html/body/div[2]/form/div[2]/div/button").click() # Enter
 
+# Commain chain
 def book(facility, slot, username, password):
-	login(username,password)
-	if facility == "gym":
-		gym(slot)
-	elif facility == "pool":
-		pool(slot)
+
+	login(username,password) # Logs in
+	choosefacility(facility) # Navigates to desired facility
+
+	print("Freeze until 7AM")
+	pause.until(botrun) # Pauses until the system time is 7am and 1 second
+
+	booked = False # Loop that will keep on trying to book until successful
+	while booked == False:
+		booked = pickslot(slot)
+	print("Slot booked")
 
 book(facility, slot, username, password)
